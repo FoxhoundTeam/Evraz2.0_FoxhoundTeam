ac2a442c0b8da6758426a5122ec50b4c924490fd
Petrov Anton <a.petrov@bi.zone>
2023-02-19 01:16:30 +0300

    Adds prediction route

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from . import message
+from . import message, prediction
 
 router = APIRouter(prefix="/api")
 router.include_router(message.router)
+router.include_router(prediction.router)
